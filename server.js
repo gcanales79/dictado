df520c4008ecf65b3c89ba2c4ab9ecb35cd13da0
Gus <gustavo.canales@me.com>
2020-10-16 15:00:43 +0200

    put port 3001

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs  = require('express-handlebars');
 
 const app=express();
 
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 3001;
 
 // Imports the Google Cloud client library
 const textToSpeech = require('@google-cloud/text-to-speech');
